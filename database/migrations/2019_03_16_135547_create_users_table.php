b843a1704ab9077873f31b49b186b5f0f2969876
Cone Pi <pikanglong@gmail.com>
2019-03-16 22:54:44 +0800

    add database migrations

--- database/migrations/2019_03_16_135547_create_users_table.php
+++ database/migrations/2019_03_16_135547_create_users_table.php
@@ -14,13 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('uid');
+            $table->string('sid');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 
---
+++ database/migrations/2019_03_16_144112_create_problem_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProblemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('problem', function (Blueprint $table) {
+            $table->increments('pid');
+            $table->string('title');
+            $table->text('description');
+            $table->string('tag');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('problem');
+    }
+}
