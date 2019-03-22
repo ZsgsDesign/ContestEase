64814b3a4e69b33f6479ae963fbcb3a067e71570
Cone Pi <pikanglong@gmail.com>
2019-03-22 22:00:29 +0800

    bug fixed

--- database/migrations/2019_03_22_215654_add_password_to_users_table.php
+++ database/migrations/2019_03_22_215654_add_password_to_users_table.php
@@ -14,7 +14,7 @@ class AddPasswordToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table -> integer('password') -> after('name');
+            $table -> string('password') -> after('name');
         });
     }
 
